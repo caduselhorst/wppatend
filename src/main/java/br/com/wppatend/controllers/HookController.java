366150e70c2a8df0d6f10f71cbe42a6ac5f49e61
carlos.clayton <carlosclayton@gmail.com>
2020-06-03 10:38:19 -0300

    Conversao body request em objeto java

--- src/main/java/br/com/wppatend/controllers/HookController.java
+++ src/main/java/br/com/wppatend/controllers/HookController.java
@@ -1,17 +1,22 @@
 package br.com.wppatend.controllers;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
+import br.com.wppatend.wpprequest.model.WppObjectRequest;
+
 @RestController
 public class HookController {
 	
+	private static final Logger logger = LoggerFactory.getLogger(HookController.class);
+	
 	@RequestMapping(value = "/hook", method = RequestMethod.POST)
-	public String hook (@RequestBody String msg) {
-		System.out.println(msg);
-		return "OK";
+	public void hook (@RequestBody WppObjectRequest msg) {
+		logger.info(String.format("Mensagem de: %1$s (%2$s) fromMe: %3$s", msg.getMessages().get(0).getChatName(), msg.getMessages().get(0).getAuthor()));
 	}
 
 }
